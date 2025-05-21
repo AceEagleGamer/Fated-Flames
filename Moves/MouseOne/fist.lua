5a2749af98970609c5d4b27ca3a1d28656eeec8b
spiralism <AceEagleGamer@github.com>
2025-05-20 20:35:18 -0600

    extra commit inserting replica for future use

---
+++ Moves/MouseOne/fist.lua
@@ -0,0 +1,22 @@
+-- boilerplate for future moves probably
+-- i'd like to make every move modular. i think it will be easier
+
+--- Public Variables ---
+local MoveData = {}
+MoveData.comboString = 0
+
+MoveData.properties = {
+    
+}
+
+--- Public Functions
+function MoveData:Tick()
+
+end
+
+function MoveData:Work()
+
+
+end
+
+return MoveData
--- src/client/input.lua
+++ src/client/input.lua
@@ -1,3 +1,26 @@
-local InputController = {}
+--- References ---
+local Player = game.Players.LocalPlayer
+local uis = game:GetService("UserInputService")
+local cas = game:GetService("ContextActionService")
 
-return InputController
+--- Public Variables ---
+local Input = {}
+Input.context = nil
+
+Input.bindings = {}
+
+--- Public Functions ---
+function Input:Init(context)
+    self.context = context
+
+    -- TODO: load actual bindings here
+    Input.bindings.MouseOne = "fist"
+end
+
+function Input:Start()
+
+    -- test i guess
+    
+end
+
+return Input
